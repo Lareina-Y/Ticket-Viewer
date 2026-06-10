1a7f0c62ce2c9f3d9649d9e8393e07aeac78c75e
Lareina <fairy3219889700@gmail.com>
2026-06-10 08:26:09 -0300

    init backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(dbConfig),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gisdb',
+  autoLoadEntities: true,
+  synchronize: false,
+};
